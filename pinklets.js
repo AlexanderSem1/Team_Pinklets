52c47664093ce3e569a1b730ad71fd750004adf1
Merethe <merethe.myrhaug@gmail.com>
2019-04-11 14:30:37 +0200

    Fourth edition

--- pinklets.js
+++ pinklets.js
@@ -164,4 +164,104 @@ var slide_index = 1;
             slides[slide_index - 1].style.display = "block";  
         }  
 
+
+        function hoverOff() {
+            var x = document.getElementById("nyhetH");
+            var y = document.getElementById("nyhetImg");
+            if (x.style.color === "black") {
+                x.style.color = "#FF5F5F";
+                y.style.opacity = "0.7";
+                
+            } else {
+                x.style.color = "black";
+                y.style.opacity = "1";
+                x.style.textDecoration = "none";
+              
+            }
+        }
+        
+        function hoverEffect()  {
+            var x = document.getElementById("nyhetH");
+            var y = document.getElementById("nyhetImg");
+            if (x.style.color === "#FF5F5F") {
+                x.style.color = "black";
+                x.style.transition = "0.5s";
+                y.style.opacity = "1";
+                y.style.transition = "0.5s";
+            } else {
+                x.style.color = "#FF5F5F";
+                y.style.opacity = "0.7";
+                x.style.transition = "0.5s";
+                y.style.transition = "0.5s";
+                x.style.textDecoration = "underline";
+              
+            }
+        }
+
+function hoverOff1() {
+    var x = document.getElementById("nyhetH1");
+    var y = document.getElementById("nyhetImg1");
+    if (x.style.color === "black") {
+        x.style.color = "#FF5F5F";
+        y.style.opacity = "0.7";
+        
+    } else {
+        x.style.color = "black";
+        y.style.opacity = "1";
+        x.style.textDecoration = "none";
+      
+    }
+}
+
+function hoverEffect1()  {
+    var x = document.getElementById("nyhetH1");
+    var y = document.getElementById("nyhetImg1");
+    if (x.style.color === "#FF5F5F") {
+        x.style.color = "black";
+        x.style.transition = "0.5s";
+        y.style.opacity = "1";
+        y.style.transition = "0.5s";
+    } else {
+        x.style.color = "#FF5F5F";
+        y.style.opacity = "0.7";
+        x.style.transition = "0.5s";
+        y.style.transition = "0.5s";
+        x.style.textDecoration = "underline";
+      
+    }
+}
+
+function hoverOff2() {
+    var x = document.getElementById("nyhetH2");
+    var y = document.getElementById("nyhetImg2");
+    if (x.style.color === "black") {
+        x.style.color = "#FF5F5F";
+        y.style.opacity = "0.7";
+        
+    } else {
+        x.style.color = "black";
+        y.style.opacity = "1";
+        x.style.textDecoration = "none";
+      
+    }
+}
+
+function hoverEffect2()  {
+    var x = document.getElementById("nyhetH2");
+    var y = document.getElementById("nyhetImg2");
+    if (x.style.color === "#FF5F5F") {
+        x.style.color = "black";
+        x.style.transition = "0.5s";
+        y.style.opacity = "1";
+        y.style.transition = "0.5s";
+    } else {
+        x.style.color = "#FF5F5F";
+        y.style.opacity = "0.7";
+        x.style.transition = "0.5s";
+        y.style.transition = "0.5s";
+        x.style.textDecoration = "underline";
+      
+    }
+}
+
 // Merethes kode slutt 
